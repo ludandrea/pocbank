cfe101d8197cef93deddd7ae07eb8e1f4a22f2d1
ludandrea <uludandrea@gmail.com>
2020-02-10 17:05:45 -0300

    first commit

---
+++ auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+    
+    isAuthenticated() {
+        
+        const token = localStorage.getItem('cadastro');
+
+        if (token) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(public dialog: MatDialog) {}
+  title = 'internet-bank-poc';
+}
